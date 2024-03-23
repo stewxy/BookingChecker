c7f30b86062da2bd09caf2d5ae6558a19ebe1d17
Stephen <stewxy13@gmail.com>
2024-03-23 15:51:11 +1300

    print and strip title, company and location of jobs ONLY with "python" in the title, in id="ResultsContainer" and under class_="card-content" Note: Must get parent div of all elements you want to access or else program will return a NoneType error since variable python_jobs only accesses the "h2" element. Parent layer starts from 0?

--- program.py
+++ program.py
@@ -8,7 +8,11 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="ResultsContainer")
 job_elements = results.find_all("div", class_="card-content")
-for job_element in job_elements:
+
+python_jobs = results.find_all("h2", string=lambda text: "python" in text.lower())
+python_job_elements = [h2_element.parent.parent.parent for h2_element in python_jobs]
+
+for job_element in python_job_elements:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
     location_element = job_element.find("p", class_="location")
