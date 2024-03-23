9bf9b015b1b546b14ac6bb27ce8df72d3257cf09
Stephen <stewxy13@gmail.com>
2024-03-23 15:52:21 +1300

    print title, company, location and links of python jobs

--- program.py
+++ program.py
@@ -20,6 +20,10 @@ for job_element in python_job_elements:
     print(company_element.text.strip())
     print(location_element.text.strip())
     print()
+    links = job_element.find_all("a")
+    for link in links:
+        link_url = link["href"]
+        print(f"Apply here: {link_url}\n".strip())
     #print(job_element, end="\n"*2)
 
 #print(results.prettify())
