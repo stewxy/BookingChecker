95f251efc3cffcc6faca41f6a23d0446315519ac
Stephen <stewxy13@gmail.com>
2024-03-24 18:37:15 +1300

    using authentication update to get html of license booking website, requires CAPTCHA action

--- test.py
+++ test.py
@@ -1,16 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
+# LicenseNumber, LicenseVersion, LastName, DateOfBirth
+
+login_url = 'https://online.nzta.govt.nz/api/authentication'
+#'https://online.nzta.govt.nz/licence-test/identification'
+
 payload = {
-    'LicenseNumber': '...',
-    'LicenseVersion': '...',
-    'LastName': '...',
-    'DateOfBirth': '...'
+    'LicenceNumber': 'ea050089',
+    'LicenceVersion': '889',
+    'LastName': 'wong',
+    'DateOfBirth': '01-09-2001'
 }
 
-with requests.Session() as s:
-    p = s.post('https://online.nzta.govt.nz/licence-test/identification', data=payload)
-    print(p.text)
+headers_ = {
+    "Content-Type": "application/json"
+}
 
-    r = s.get('https://online.nzta.govt.nz/licence-test/booking/eligibility')
-    print(r.text)
+r = requests.post(login_url, json=payload, headers=headers_)
+print(r.text)
+print(r.status_code)
+# print(r.content)
+
+# with requests.Session() as s:
+#     p = s.post(login_url, data=payload)
+#     print(p.text)
+#     print(p.status_code)
+#     print(p.content)
+#
+#     r = s.get('https://online.nzta.govt.nz/licence-test/booking/eligibility')
+#     print(r.text)
