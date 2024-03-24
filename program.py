29477d4646035a46981bbd3d208f8a41ab551ef1
Stephen <stewxy13@gmail.com>
2024-03-24 17:38:21 +1300

    print title, company, location and link to apply of python jobs

--- program.py
+++ program.py
@@ -4,13 +4,15 @@ from bs4 import BeautifulSoup
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
-#print(page.text)
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="ResultsContainer")
 job_elements = results.find_all("div", class_="card-content")
 
 python_jobs = results.find_all("h2", string=lambda text: "python" in text.lower())
 python_job_elements = [h2_element.parent.parent.parent for h2_element in python_jobs]
+
+test_links = results.find_all("a")
+print(test_links, "***")
 
 for job_element in python_job_elements:
     title_element = job_element.find("h2", class_="title")
@@ -21,9 +23,8 @@ for job_element in python_job_elements:
     print(location_element.text.strip())
     print()
     links = job_element.find_all("a")
-    for link in links:
-        link_url = link["href"]
-        print(f"Apply here: {link_url}\n".strip())
-    #print(job_element, end="\n"*2)
 
-#print(results.prettify())
+    apply_link = [link for link in links if link["href"] !="https://www.realpython.com"]
+    print("Apply here:", apply_link[0]["href"],  "\n")
+
+    # link_url = job_element.find_all("a")[1]["href"]
