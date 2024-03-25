6a494e0ea7d3eb3edd7887384e05c4fb8ed08079
Stephen <stewxy13@gmail.com>
2024-03-25 17:24:11 +1300

    using selenium to login

--- test.py
+++ test.py
@@ -1,25 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver import Chrome, Keys
+from selenium.webdriver.common.by import By
 
-# LicenseNumber, LicenseVersion, LastName, DateOfBirth
-
-login_url = 'https://online.nzta.govt.nz/api/authentication'
-#'https://online.nzta.govt.nz/licence-test/identification'
 
 payload = {
-    'LicenceNumber': 'ea050089',
-    'LicenceVersion': '889',
-    'LastName': 'wong',
-    'DateOfBirth': '01-09-2001'
+    'LicenceNumber': '...',
+    'LicenceVersion': '...',
+    'LastName': '...',
+    'DateOfBirth': '...',
 }
 
 headers_ = {
     "Content-Type": "application/json"
 }
 
-r = requests.post(login_url, json=payload, headers=headers_)
-print(r.text)
-print(r.status_code)
+login_url = 'https://online.nzta.govt.nz/licence-test/identification'
+
+opts = webdriver.ChromeOptions()
+opts.headless = True
+driver = Chrome(options=opts)
+
+assert opts.headless
+browser = Chrome(options=opts)
+browser.get(login_url)
+
+licence_number = browser.find_element(by=By.XPATH, value="//input[@formcontrolname='LicenceNumber']")
+licence_version = browser.find_element(by=By.XPATH, value="//input[@formcontrolname='LicenceVersion']")
+last_name = browser.find_element(by=By.XPATH, value="//input[@formcontrolname='LastName']")
+date_of_birth = browser.find_element(by=By.XPATH, value="//input[@formcontrolname='DateOfBirth']")
+
+licence_number.send_keys(payload.get('LicenceNumber') + Keys.TAB + payload.get('LicenceVersion') + Keys.TAB + payload.get('LastName') + Keys.TAB + payload.get('DateOfBirth') + Keys.ENTER)
+
+while True:
+    pass
+
+
+# print(browser.text)
+# browser.close()
+# quit()
+
+# r = requests.post(login_url, json=payload, headers=headers_)
+# print(r.text)
+# print(r.status_code)
+
 # print(r.content)
 
 # with requests.Session() as s:
